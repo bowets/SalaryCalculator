9face9484b0790c6906f6ff6fb862c75a024839c
Borna Forsek <bforsek@gmail.com>
2023-11-12 10:20:03 +0100

    Update Prsi.class - todo: add threshold and percentage

--- src/salarycalculator/tax/Prsi.java
+++ src/salarycalculator/tax/Prsi.java
@@ -2,4 +2,34 @@ package salarycalculator.tax;
 
 public class Prsi {
 
+	private float[] prsiPercentage;
+	private float[] prsiIncomeThreshold;
+	private float minimumPrsiThreshold;
+	
+	private boolean qualifiesForPrsi(float income) {
+		if (income < this.minimumPrsiThreshold) {
+			return false;
+		}
+		return true;
+	}
+	
+	public float prsiDue(float income) {
+		float PRSI = 0;
+		if(!this.qualifiesForPrsi(income)) {
+			return PRSI;
+		} else {
+		
+		for (int i = this.prsiIncomeThreshold.length-1; i >= 0; i--) {
+			if(income > this.prsiIncomeThreshold[i]) {
+				float taxableDifference = income - this.prsiIncomeThreshold[i];
+				PRSI += taxableDifference * this.prsiPercentage[i];
+				income = income - taxableDifference;
+			}
+		}
+		
+		
+		return PRSI;
+		}
+	}
+	
 }
